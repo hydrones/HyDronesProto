c1c71d78b07bd4ee591742533caa2d43876feb21
Jean-Christophe Poisson <Jean-Christophe Poisson>
2016-10-11 23:24:49 +0200

    Creation du mode 1 d'acquisition haute résolution du prototype HyDrones MK1

--- HyDrones_DryRun.py
+++ HyDrones_DryRun.py
@@ -15,8 +15,43 @@ import datetime as dt
 import struct
 
 
-# HD = hd.Hydrones('/dev/ttyACM0', 115200)
-HD = hd.Hydrones('COM3', 115200)
+
+
+def gpsMeasurement():
+     gpsMeas = HD.doGPSMeas()
+     print('\n')
+     print("GPS=> Date: %d/%d/%d %s, Lat: %f, Lon: %f, GeoidHeight: %f, NbSat: %d, Altitude: %f m" % (gpsMeas['Day'], gpsMeas['Month'], gpsMeas['Year'], gpsMeas['Time'], gpsMeas['Latitude'], gpsMeas['Longitude'], gpsMeas['GeoidHeight'], gpsMeas['NbSat'], gpsMeas['Altitude']))
+    #  now = dt.datetime.now()
+    #  print("GPS ==>"+str(now))
+
+def leddarMeasurement():
+    leddarMeas = HD.doLeddarMeas()
+    print("Leddar=> Amplitude: %f, Distance: %f m"% (leddarMeas['Ampl'], leddarMeas['Range']))
+    # now = dt.datetime.now()
+    # print("Leddar ==>"+str(now))
+
+
+def baroMeasurement():
+    baroMeas = HD.doBaroMeas()
+    print("Barometer=> Pressure: %f Pa, Pressure at sea level: %f Pa, Altitude: %f m, Temperature: %f C" % (baroMeas['Pressure'], baroMeas['SeaLevelPressure'], baroMeas['Altitude'], baroMeas['Temperature']))
+    # now = dt.datetime.now()
+    # print("Baro ==>"+str(now))
+
+def imuMeasurement():
+    imuMeas = HD.doIMUMeas()
+    print("IMU Angle=> Pitch angle: %f deg, Roll angle: %f deg, Yaw angle: %f deg"% (imuMeas['PitchAngle'], imuMeas['RollAngle'], imuMeas['YawAngle']))
+    print("IMU Accel=> Axel X: %f m/s-2, Accel Y: %f m/s-2, Accel Z: %f m/s-2"% (imuMeas['AccelX'], imuMeas['AccelY'], imuMeas['AccelZ']))
+    print("IMU Linear Accel=> Linear Axel X: %f m/s-2, Linear Accel Y: %f m/s-2, Linear Accel Z: %f m/s-2"% (imuMeas['LinearAccelX'], imuMeas['LinearAccelY'], imuMeas['LinearAccelZ']))
+    print("IMU Gravity Accel=> Gravity Axel X: %f g, Gravity Accel Y: %f g, Gravity Accel Z: %f g"% (imuMeas['GravAccelX'], imuMeas['GravAccelY'], imuMeas['GravAccelZ']))
+    # now = dt.datetime.now()
+    # print("IMU ==>"+str(now))
+
+
+
+HD = hd.Hydrones('/dev/ttyACM0', 115200)
+# HD = hd.Hydrones('COM3', 115200)
+
+start = dt.datetime.now()
 
 flagFixGPS = False
 isDataFileOpen = False
@@ -34,32 +69,55 @@ while True:
     # Start HyDrones Measure if fix GPS = OK
     if (flagFixGPS):
 
-        now = dt.datetime.now()
-
         # Send a command to the HyDrones Proto ==> "do a GPS Measure"
-        gpsMeas = HD.doGPSMeas()
+        # gpsMeas = HD.doGPSMeas()
+        # gpsMeasurement()
 
         # Send a command to the HyDrones Proto ==> "do a leddar Measure"
-        leddarMeas = HD.doLeddarMeas()
+        # leddarMeas = HD.doLeddarMeas()
+        # leddarMeasurement()
 
         # Send a command to the HyDrones Proto ==> "do a barometer Measure"
-        baroMeas = HD.doBaroMeas()
+        # baroMeas = HD.doBaroMeas()
+        # baroMeasurement()
 
         # Send a command to the HyDrones Proto ==> "do a IMU Measure"
-        imuMeas = HD.doIMUMeas()
+        # imuMeas = HD.doIMUMeas()
+        # imuMeasurement()
 
         # Date convertion
-        dateBuffer = str(gpsMeas['Year'])+'/'+str(gpsMeas['Month'])+'/'+str(gpsMeas['Day'])+' '+gpsMeas['Time']
-        currentDate = dt.datetime.strptime(dateBuffer, "%y/%m/%d %H:%M:%S.%f")
+        # dateBuffer = str(gpsMeas['Year'])+'/'+str(gpsMeas['Month'])+'/'+str(gpsMeas['Day'])+' '+gpsMeas['Time']
+        # currentDate = dt.datetime.strptime(dateBuffer, "%y/%m/%d %H:%M:%S.%f")
 
 
-        # Print results
-        print("GPS=> Date: %d/%d/%d %s, Lat: %f, Lon: %f, GeoidHeight: %f, NbSat: %d, Altitude: %f m" % (gpsMeas['Day'], gpsMeas['Month'], gpsMeas['Year'], gpsMeas['Time'], gpsMeas['Latitude'], gpsMeas['Longitude'], gpsMeas['GeoidHeight'], gpsMeas['NbSat'], gpsMeas['Altitude']))
-        print("Leddar=> Amplitude: %f, Distance: %f m"% (leddarMeas['Ampl'], leddarMeas['Range']))
-        print("Barometer=> Pressure: %f Pa, Pressure at sea level: %f Pa, Altitude: %f m, Temperature: %f C" % (baroMeas['Pressure'], baroMeas['SeaLevelPressure'], baroMeas['Altitude'], baroMeas['Temperature']))
-        print("IMU Angle=> Pitch angle: %f deg, Roll angle: %f deg, Yaw angle: %f deg"% (imuMeas['PitchAngle'], imuMeas['RollAngle'], imuMeas['YawAngle']))
-        print("IMU Accel=> Axel X: %f m/s-2, Accel Y: %f m/s-2, Accel Z: %f m/s-2"% (imuMeas['AccelX'], imuMeas['AccelY'], imuMeas['AccelZ']))
-        print("IMU Linear Accel=> Linear Axel X: %f m/s-2, Linear Accel Y: %f m/s-2, Linear Accel Z: %f m/s-2"% (imuMeas['LinearAccelX'], imuMeas['LinearAccelY'], imuMeas['LinearAccelZ']))
-        print("IMU Gravity Accel=> Gravity Axel X: %f g, Gravity Accel Y: %f g, Gravity Accel Z: %f g"% (imuMeas['GravAccelX'], imuMeas['GravAccelY'], imuMeas['GravAccelZ']))
-        # print(now)
-        print("-----------------------\n")
+        # Test d'un schema de mesure
+        gpsMeasurement()
+        baroMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        imuMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        imuMeasurement()
+        baroMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        imuMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+        imuMeasurement()
+        leddarMeasurement()
+        leddarMeasurement()
+
+        current = dt.datetime.now() - start
+        print(current.total_seconds())
+
+
--- HyDrones_Proto.py
+++ HyDrones_Proto.py
@@ -16,9 +16,12 @@ import struct
 
 
 HD = hd.Hydrones('/dev/ttyACM0', 115200)
+# HD = hd.Hydrones('COM3', 115200)
+
 
 flagFixGPS = False
 isDataFileOpen = False
+startclock = dt.datetime.now()
 while True:
 
     # Initialize the GPS signal until the satellite's hooking
@@ -35,23 +38,86 @@ while True:
 
         # Send a command to the HyDrones Proto ==> "do a GPS Measure"
         gpsMeas = HD.doGPSMeas()
-
-        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
-        leddarMeas = HD.doLeddarMeas()
-
-        # Send a command to the HyDrones Proto ==> "do a barometer Measure"
-        baroMeas = HD.doBaroMeas()
-
-        # Send a command to the HyDrones Proto ==> "do a IMU Measure"
-        imuMeas = HD.doIMUMeas()
-
+        timetagGPS = (dt.datetime.now() - startclock).total_seconds()
         # Date convertion
         dateBuffer = str(gpsMeas['Year'])+'/'+str(gpsMeas['Month'])+'/'+str(gpsMeas['Day'])+' '+gpsMeas['Time']
         currentDate = dt.datetime.strptime(dateBuffer, "%y/%m/%d %H:%M:%S.%f")
 
+        # Send a command to the HyDrones Proto ==> "do a barometer Measure"
+        baroMeas01 = HD.doBaroMeas()
+        timetagBaro01 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
+        leddarMeas01 = HD.doLeddarMeas()
+        timetagLeddar01 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas02 = HD.doLeddarMeas()
+        timetagLeddar02 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas03 = HD.doLeddarMeas()
+        timetagLeddar03 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas04 = HD.doLeddarMeas()
+        timetagLeddar04 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a IMU Measure"
+        imuMeas01 = HD.doIMUMeas()
+        timetagIMU01 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
+        leddarMeas05 = HD.doLeddarMeas()
+        timetagLeddar05 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas06 = HD.doLeddarMeas()
+        timetagLeddar06 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas07 = HD.doLeddarMeas()
+        timetagLeddar07 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas08 = HD.doLeddarMeas()
+        timetagLeddar08 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a IMU Measure"
+        imuMeas02 = HD.doIMUMeas()
+        timetagIMU02 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a barometer Measure"
+        baroMeas02 = HD.doBaroMeas()
+        timetagBaro02 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
+        leddarMeas09 = HD.doLeddarMeas()
+        timetagLeddar09 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas10 = HD.doLeddarMeas()
+        timetagLeddar10 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas11 = HD.doLeddarMeas()
+        timetagLeddar11 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas12 = HD.doLeddarMeas()
+        timetagLeddar12 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a IMU Measure"
+        imuMeas03 = HD.doIMUMeas()
+        timetagIMU03 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
+        leddarMeas13 = HD.doLeddarMeas()
+        timetagLeddar13 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas14 = HD.doLeddarMeas()
+        timetagLeddar14 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas15 = HD.doLeddarMeas()
+        timetagLeddar15 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas16 = HD.doLeddarMeas()
+        timetagLeddar16 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a IMU Measure"
+        imuMeas04 = HD.doIMUMeas()
+        timetagIMU04 = (dt.datetime.now() - startclock).total_seconds()
+
+        # Send a command to the HyDrones Proto ==> "do a leddar Measure"
+        leddarMeas17 = HD.doLeddarMeas()
+        timetagLeddar17 = (dt.datetime.now() - startclock).total_seconds()
+        leddarMeas18 = HD.doLeddarMeas()
+        timetagLeddar18 = (dt.datetime.now() - startclock).total_seconds()
+
+
         # Struct data for the binary file
-        structMeas = struct.Struct("<HBBBBBIfffIffIfffffffff")
-        packedMeas = structMeas.pack(currentDate.year, \
+        structMeas = struct.Struct("<dH5BI3fIfd4fdfIdfIdfIdfId12fdfIdfIdfIdfId12fd4fdfIdfIdfIdfId12fdfIdfIdfIdfId12fdfIdfI")
+        packedMeas = structMeas.pack(timetagGPS, \
+                                     currentDate.year, \
                                      currentDate.month, \
                                      currentDate.day, \
                                      currentDate.hour, \
@@ -63,22 +129,127 @@ while True:
                                      gpsMeas['GeoidHeight'], \
                                      gpsMeas['NbSat'], \
                                      gpsMeas['Altitude'], \
-                                     leddarMeas['Range'], \
-                                     leddarMeas['Ampl'], \
-                                     baroMeas['Pressure'], \
-                                     baroMeas['SeaLevelPressure'], \
-                                     baroMeas['Altitude'], \
-                                     baroMeas['Temperature'], \
-                                     imuMeas['PitchAngle'], \
-                                     imuMeas['RollAngle'], \
-                                     imuMeas['LinearAccelX'], \
-                                     imuMeas['LinearAccelY'], \
-                                     imuMeas['LinearAccelZ'])
+                                     timetagBaro01, \
+                                     baroMeas01['Pressure'], \
+                                     baroMeas01['SeaLevelPressure'], \
+                                     baroMeas01['Altitude'], \
+                                     baroMeas01['Temperature'], \
+                                     timetagLeddar01, \
+                                     leddarMeas01['Range'], \
+                                     leddarMeas01['Ampl'], \
+                                     timetagLeddar02, \
+                                     leddarMeas02['Range'], \
+                                     leddarMeas02['Ampl'], \
+                                     timetagLeddar03, \
+                                     leddarMeas03['Range'], \
+                                     leddarMeas03['Ampl'], \
+                                     timetagLeddar04, \
+                                     leddarMeas04['Range'], \
+                                     leddarMeas04['Ampl'], \
+                                     timetagIMU01, \
+                                     imuMeas01['PitchAngle'], \
+                                     imuMeas01['RollAngle'], \
+                                     imuMeas01['YawAngle'], \
+                                     imuMeas01['AccelX'], \
+                                     imuMeas01['AccelY'], \
+                                     imuMeas01['AccelZ'], \
+                                     imuMeas01['LinearAccelX'], \
+                                     imuMeas01['LinearAccelY'], \
+                                     imuMeas01['LinearAccelZ'], \
+                                     imuMeas01['GravAccelX'], \
+                                     imuMeas01['GravAccelY'], \
+                                     imuMeas01['GravAccelZ'], \
+                                     timetagLeddar05, \
+                                     leddarMeas05['Range'], \
+                                     leddarMeas05['Ampl'], \
+                                     timetagLeddar06, \
+                                     leddarMeas06['Range'], \
+                                     leddarMeas06['Ampl'], \
+                                     timetagLeddar07, \
+                                     leddarMeas07['Range'], \
+                                     leddarMeas07['Ampl'], \
+                                     timetagLeddar08, \
+                                     leddarMeas08['Range'], \
+                                     leddarMeas08['Ampl'], \
+                                     timetagIMU02, \
+                                     imuMeas02['PitchAngle'], \
+                                     imuMeas02['RollAngle'], \
+                                     imuMeas02['YawAngle'], \
+                                     imuMeas02['AccelX'], \
+                                     imuMeas02['AccelY'], \
+                                     imuMeas02['AccelZ'], \
+                                     imuMeas02['LinearAccelX'], \
+                                     imuMeas02['LinearAccelY'], \
+                                     imuMeas02['LinearAccelZ'], \
+                                     imuMeas02['GravAccelX'], \
+                                     imuMeas02['GravAccelY'], \
+                                     imuMeas02['GravAccelZ'], \
+                                     timetagBaro02, \
+                                     baroMeas02['Pressure'], \
+                                     baroMeas02['SeaLevelPressure'], \
+                                     baroMeas02['Altitude'], \
+                                     baroMeas02['Temperature'], \
+                                     timetagLeddar09, \
+                                     leddarMeas09['Range'], \
+                                     leddarMeas09['Ampl'], \
+                                     timetagLeddar10, \
+                                     leddarMeas10['Range'], \
+                                     leddarMeas10['Ampl'], \
+                                     timetagLeddar11, \
+                                     leddarMeas11['Range'], \
+                                     leddarMeas11['Ampl'], \
+                                     timetagLeddar12, \
+                                     leddarMeas12['Range'], \
+                                     leddarMeas12['Ampl'], \
+                                     timetagIMU03, \
+                                     imuMeas03['PitchAngle'], \
+                                     imuMeas03['RollAngle'], \
+                                     imuMeas03['YawAngle'], \
+                                     imuMeas03['AccelX'], \
+                                     imuMeas03['AccelY'], \
+                                     imuMeas03['AccelZ'], \
+                                     imuMeas03['LinearAccelX'], \
+                                     imuMeas03['LinearAccelY'], \
+                                     imuMeas03['LinearAccelZ'], \
+                                     imuMeas03['GravAccelX'], \
+                                     imuMeas03['GravAccelY'], \
+                                     imuMeas03['GravAccelZ'], \
+                                     timetagLeddar13, \
+                                     leddarMeas13['Range'], \
+                                     leddarMeas13['Ampl'], \
+                                     timetagLeddar14, \
+                                     leddarMeas14['Range'], \
+                                     leddarMeas14['Ampl'], \
+                                     timetagLeddar15, \
+                                     leddarMeas15['Range'], \
+                                     leddarMeas15['Ampl'], \
+                                     timetagLeddar16, \
+                                     leddarMeas16['Range'], \
+                                     leddarMeas16['Ampl'], \
+                                     timetagIMU04, \
+                                     imuMeas04['PitchAngle'], \
+                                     imuMeas04['RollAngle'], \
+                                     imuMeas04['YawAngle'], \
+                                     imuMeas04['AccelX'], \
+                                     imuMeas04['AccelY'], \
+                                     imuMeas04['AccelZ'], \
+                                     imuMeas04['LinearAccelX'], \
+                                     imuMeas04['LinearAccelY'], \
+                                     imuMeas04['LinearAccelZ'], \
+                                     imuMeas04['GravAccelX'], \
+                                     imuMeas04['GravAccelY'], \
+                                     imuMeas04['GravAccelZ'], \
+                                     timetagLeddar17, \
+                                     leddarMeas17['Range'], \
+                                     leddarMeas17['Ampl'], \
+                                     timetagLeddar18, \
+                                     leddarMeas18['Range'], \
+                                     leddarMeas18['Ampl'])
 
         # Data record into binary files
         if (isDataFileOpen == False):
             startDate = currentDate
-            fileName = "data/HD_test_"+startDate.strftime("%Y%m%d_%H%M%S%f")
+            fileName = "data/HD_mode1_"+startDate.strftime("%Y%m%d_%H%M%S%f")
             binaryFile = open(fileName, "wb")
             isDataFileOpen = True
         else:
